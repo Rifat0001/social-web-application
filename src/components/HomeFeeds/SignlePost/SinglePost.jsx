3d19c08f24f685f8acbe93810e968529f76a0a98
muhiminulalvi <eee1916039brur@gmail.com>
2023-06-23 11:11:30 +0600

    post first section added

--- src/components/HomeFeeds/SignlePost/SinglePost.jsx
+++ src/components/HomeFeeds/SignlePost/SinglePost.jsx
@@ -1,11 +1,72 @@
-import React from 'react';
+import Img from "../../../assets/CreatePost/bd-person.png";
+import {
+  FaAngleDown,
+  FaArrowRight,
+  FaEllipsisH,
+  FaGlobe,
+  FaHeart,
+  FaRegComments,
+} from "react-icons/fa";
+import { HiOutlineBookmarkAlt } from "react-icons/hi";
+import "./SinglePost.css";
+import Sicon from "../../../assets/CreatePost/Sicon.png";
 
 const SinglePost = () => {
-    return (
-        <div>
+  return (
+    <div className="bg-[#0A123E] py-5 px-6 mx-6 my-6 text-white">
+      <div className="flex items-start justify-between">
+        <div className="flex items-center justify-start gap-5">
+          <img src={Img} alt="" />
 
+          <div className="flex-col">
+            <p className="flex items-center justify-start gap-2">
+              <span>Oblivion Isme</span>
+              <FaArrowRight />
+              <span>Kevin Alexander</span>
+              <span className="text-[16px] profile-user">@kevinalexa3546</span>
+            </p>
+            <p className="flex items-center gap-3">
+              <span className="text-[16px] profile-user">@Oblivion1122</span>
+              <span>now</span>
+              <FaGlobe />
+              <FaAngleDown />
+            </p>
+          </div>
+          <div></div>
         </div>
-    );
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <FaEllipsisH size={30} />
+          </button>
+        </div>
+      </div>
+      <div className="space-y-[40px]">
+        <h2 className=" ps-[105px]">happy birthdayyyy</h2>
+        <div className="text-end">
+          <div className="stats shadow bg-[rgba(255,255,255,0.10)] rounded-[12px] border-y-[1px]">
+            <div className="flex items-center justify-center gap-3 px-2">
+              <div className=" text-white">200.8K</div>
+              <FaHeart color="#FF0000" />
+            </div>
+            <div className="pro-side flex justify-center items-center gap-5 px-2">
+              <h2 className="text-white">4.2K</h2>
+              <FaRegComments color="#3ADF00"/>
+            </div>
+            <div className="pro-side flex justify-center items-center gap-7 px-2">
+              <h2 className="text-white">24</h2>
+              <HiOutlineBookmarkAlt color="#B82BEA"/>
+            </div>
+
+            
+            <div className="pro-side flex items-center justify-center gap-10 px-2">
+            <h2 className="text-white">1</h2>
+              <img src={Sicon} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SinglePost;
