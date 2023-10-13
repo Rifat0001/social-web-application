d74bad68d2541e6e0e2b95878cb7ec1949684743
Rifat0001 <mahmudrifat764@gmail.com>
2023-10-14 04:49:31 +0600

    profile left part end

---
+++ src/ProfileParts/ProfileContains/PCategroy.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+const PCategroy = () => {
+    return (
+        <div className='category-page-profile'>
+            d
+        </div>
+    );
+};
+
+export default PCategroy;
--- src/ProfileParts/ProfileMainColumn/ProfileMainCol.jsx
+++ src/ProfileParts/ProfileMainColumn/ProfileMainCol.jsx
@@ -1,12 +1,12 @@
 import ProfileTab from '../ProfileTab/ProfileTab';
 import ProfileTop from '../ProfileTop/ProfileTop';
-import { FaBeer } from 'react-icons/fa';
+import ProMiddle from './ProMiddle';
 import './style.css'
 const ProfileMainCol = () => (
     <div className='profile-main p-4 bg-[#00072D]'>
         <ProfileTop></ProfileTop>
         <ProfileTab></ProfileTab>
-
+        <ProMiddle></ProMiddle>
     </div>
 );
 
